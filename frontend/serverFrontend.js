882293dbd68a8e54d41fd82acd227954a8c2f616
Jakob Waldmann <jakob.waldmann@mercedes-benz.com>
2025-07-09 15:05:46 +0200

    frontend server läuft. Muss aber einmal neugestartet werden.

--- frontend/serverFrontend.js
+++ frontend/serverFrontend.js
@@ -2,25 +2,13 @@
 import express from "express";
 import * as fs from "node:fs";
 import * as path from "node:path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';import morgan from "morgan";
+import morgan from "morgan";
 const app = express()
 const port = 5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(".", ".")));
-/*
-app.use(express.static(path.join(".", "pictures")));
-app.use(express.static(path.join(".", "ProductPage")));
-app.use(express.static(path.join(".", "Profilverwaltung")));
-app.use(express.static(path.join(".", "public")));
-app.use(express.static(path.join(".", "SignUpPage")));
-app.use(express.static(path.join(".", "LoginPage")));
-app.use(express.static(path.join(".", "unauthorized")));
-app.use(express.static(path.join(".", "user")));
-app.use(express.static(path.join(".", "Userverwaltung")));
- */
 
 // configure logging before each api call
 morgan.token('source', function () {
@@ -28,7 +16,6 @@ morgan.token('source', function () {
 });
 const customFormat = ':source :method :url :status :res[content-length] - :response-time ms';
 app.use(morgan(customFormat));
-
 
 // Globales abfangen unbehandelter Fehler
 app.use((err, req, res, next) => {
@@ -43,15 +30,21 @@ app.use((err, req, res, next) => {
 // dann mit res.send(htmlSend)
 app.get("/", (req, res) => {
     console.log("gelogged");
-    //res.status(200).json({message:"gelogged"});
+    renderPage('index.html','user', res)
+})
+
+app.get("/user", (req, res) => {
+    console.log("gelogged2");
     renderPage('index.html','user', res)
 })
 
 function renderPage(page, ordner, res) {
+    console.log("page rendering");
     const fullPath = path.join('.', ordner, page);
+    console.log(fullPath);
     fs.readFile(fullPath, 'utf-8', (err, content) => {
         if (err) return res.status(500).send('Seitenfehler');
-        res.send(content);
+        return res.send(content);
     });
 }
 
