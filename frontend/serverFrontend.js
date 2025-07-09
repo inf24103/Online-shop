6fbbbdfaea1404c06838aa987e591d17f5b05eae
Jakob Waldmann <jakob.waldmann@mercedes-benz.com>
2025-07-09 13:07:35 +0200

    frontend server index läuft2

--- frontend/serverFrontend.js
+++ frontend/serverFrontend.js
@@ -1,17 +1,26 @@
-// neues docker compose erstellen
-// Docker 1. mal starten: docker compose up --build
-// Docker wieder löschen: docker compose down -v
 // cors wieder rein machen für die Kommunikation zum Backend Server
 import express from "express";
 import * as fs from "node:fs";
 import * as path from "node:path";
-import morgan from "morgan";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';import morgan from "morgan";
 const app = express()
 const port = 5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(".", "frontend")));
+app.use(express.static(path.join(".", ".")));
+/*
+app.use(express.static(path.join(".", "pictures")));
+app.use(express.static(path.join(".", "ProductPage")));
+app.use(express.static(path.join(".", "Profilverwaltung")));
+app.use(express.static(path.join(".", "public")));
+app.use(express.static(path.join(".", "SignUpPage")));
+app.use(express.static(path.join(".", "LoginPage")));
+app.use(express.static(path.join(".", "unauthorized")));
+app.use(express.static(path.join(".", "user")));
+app.use(express.static(path.join(".", "Userverwaltung")));
+ */
 
 // configure logging before each api call
 morgan.token('source', function () {
@@ -33,11 +42,20 @@ app.use((err, req, res, next) => {
 // bei jedem Endpunkt das html Template einlesen
 // dann mit res.send(htmlSend)
 app.get("/", (req, res) => {
-    const html = fs.readFileSync('user/index.html', 'utf8');
-    res.setHeader('Content-Type', 'text/html;charset=utf-8');
-    res.send("html");
+    console.log("gelogged");
+    //res.status(200).json({message:"gelogged"});
+    renderPage('index.html','user', res)
 })
+
+function renderPage(page, ordner, res) {
+    const fullPath = path.join('.', ordner, page);
+    fs.readFile(fullPath, 'utf-8', (err, content) => {
+        if (err) return res.status(500).send('Seitenfehler');
+        res.send(content);
+    });
+}
 
 app.listen(port, () => {
     console.log(`Frontend-Server läuft auf http://localhost:${port}`)
+    console.log("minutes: ", new Date().getMinutes(), "seconds: ",new Date().getSeconds());
 })
